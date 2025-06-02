abf1fc4d0be8dac598f8ee9515d442644857cb40
Student RM4-3073 01 <studentlab1401@it.polsl.pl>
2025-06-02 11:05:31 +0200

    Zadania 7

--- src/app/osoby.service.ts
+++ src/app/osoby.service.ts
@@ -2,35 +2,42 @@ import { Injectable } from '@angular/core';
 import { Osoba } from '../models/osoba.class';
 import { Observable, delay, of } from 'rxjs';
 import { OsobaForm } from '../models/osoba-form';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OsobyService {
-  private osoby: Osoba[] = [
-    new Osoba("Jan", "Kowalski", 33, false),
-    new Osoba("Anna", "Nowak", 44, false),
-    new Osoba("Adam", "XXX", 13, true),
-    new Osoba("Ewelina", "YYY", 41, false),
-  ];
+  // private osoby: Osoba[] = [
+  //   new Osoba("Jan", "Kowalski", 33, false),
+  //   new Osoba("Anna", "Nowak", 44, false),
+  //   new Osoba("Adam", "XXX", 13, true),
+  //   new Osoba("Ewelina", "YYY", 41, false),
+  // ];
 
-  constructor() { }
+  private readonly baseURL = 'http://localhost:5256/api/Osoby';
+
+  constructor(private httpClient: HttpClient) { }
 
   get(): Observable<Osoba[]> {
-    return of(this.osoby);
+    // return of(this.osoby);
+    return this.httpClient.get<Osoba[]>(this.baseURL);
   }
 
   getById(id: number): Observable<Osoba> {
-    const osoba = this.osoby.find(o => o.id === id);
-    if(osoba == null) {
-      throw new Error("nie znaleziono osoby o id = " + id);
-    }
-    return of(osoba).pipe(delay(1000));
+    // const osoba = this.osoby.find(o => o.id === id);
+    // if(osoba == null) {
+    //   throw new Error("nie znaleziono osoby o id = " + id);
+    // }
+    // return of(osoba).pipe(delay(1000));
+    const url = `${this.baseURL}/${id}`;
+    return this.httpClient.get<Osoba>(url);
   }
 
   post(body: OsobaForm): Observable<void> {
-    const nowaOsoba = new Osoba(body.imie, body.nazwisko, body.wiek, false);
-    this.osoby.push(nowaOsoba);
-    return of(undefined);
+    // const nowaOsoba = new Osoba(body.imie, body.nazwisko, body.wiek, false);
+    // this.osoby.push(nowaOsoba);
+    // return of(undefined);
+    return this.httpClient.post<void>(this.baseURL, body);
   }
 }
--- src/models/osoba.class.ts
+++ src/models/osoba.class.ts
@@ -1,17 +1,25 @@
-export class Osoba {
-    // imie: string;
+// export class Osoba {
+//     // imie: string;
+//     nazwisko: string;
+//     wiek: number;
+//     czyWyrozniona: boolean;
+//     id: number;
+
+//     private static idGen = 1;
+
+//     constructor(public imie: string, nazwisko: string, wiek: number, czyWyrozniona: boolean) {
+//         // this.imie = imie;
+//         this.nazwisko = nazwisko;
+//         this.czyWyrozniona = czyWyrozniona;
+//         this.wiek = wiek;
+//         this.id = Osoba.idGen++;
+//     }
+// }
+
+export interface Osoba {
+    imie: string;
     nazwisko: string;
     wiek: number;
     czyWyrozniona: boolean;
     id: number;
-
-    private static idGen = 1;
-
-    constructor(public imie: string, nazwisko: string, wiek: number, czyWyrozniona: boolean) {
-        // this.imie = imie;
-        this.nazwisko = nazwisko;
-        this.czyWyrozniona = czyWyrozniona;
-        this.wiek = wiek;
-        this.id = Osoba.idGen++;
-    }
 }
